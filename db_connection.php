3f775161aa13b6c8861a1aecb2f312e0757541aa
Paras Mani <168018735+parasmani5929@users.noreply.github.com>
2025-03-27 07:11:29 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";  // Change if MySQL runs on a different port
-$username = "root";   // Your MySQL username
-$password = "Paras@12";       // Your MySQL password (leave empty if no password)
-$dbname = "food_ordering"; // Your database name
+$username = "xxxx";   // Your MySQL username
+$password = "xxxxxxxxxxx";       // Your MySQL password (leave empty if no password)
+$dbname = "db_name"; // Your database name
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
