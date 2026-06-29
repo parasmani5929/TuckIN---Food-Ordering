1fa30eaa626391213ab3119c5f33aab460760f87
Paras Mani <168018735+parasmani5929@users.noreply.github.com>
2026-06-29 16:15:10 +0530

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -5,7 +5,7 @@ $password = "xxxxxxxxxxx";       // Your MySQL password (leave empty if no passw
 $dbname = "db_name"; // Your database name
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password); 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     echo "✅ ";
 } catch (PDOException $e) {
