487961a00537d479db255c6cf401a7f002d533cb
Paras Mani <168018735+parasmani5929@users.noreply.github.com>
2026-07-24 23:27:53 +0530

    Update adminlogin.php

--- adminlogin.php
+++ adminlogin.php
@@ -2,7 +2,7 @@
 session_start();
 include 'db_connection.php';
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] === "POST") { 
     if (!isset($_POST['email'], $_POST['password'])) {
         die("❌ Missing email or password. <a href='adminlogin.php'>Try again</a>");
     }
